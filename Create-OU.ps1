d71a16ea97137d5b1cb1d143d3c80c8f125583f8
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2026-04-23 21:40:59 -0500

    Add admin check and fix AD OU creation logic

--- Create-OU.ps1
+++ Create-OU.ps1
@@ -1,4 +1,26 @@
-﻿function Create-OU {
+﻿
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Please run this script as Administrator."
+    return 
+}
+else {
+    Write-Host "Running as Administrator."
+}
+
+function AD_Module_Installed{
+
+    if(-not(Get-Module -name ActiveDirectory )){
+        try {
+            Import-Module -Name ActiveDirectory
+            Write-Host "ActiveDirectory has been installed..."
+        }
+        catch {
+            Write-Host "Failed to Create..." -ForegroundColor Red
+        }
+    } 
+}
+
+function Create-OU {
 
     param (
         [string]$name
@@ -6,22 +28,16 @@
 
     $OUPath = "DC=Globex,DC=com"
 
-    if (Get-ADOrganizationalUnit -Filter "Name -eq '$name'" -SeachBase $OUPath){
-    
+    if (Get-ADOrganizationalUnit -Filter "Name -eq '$name'" -SearchBase $OUPath) {    
         Write-Host "Organizational Unit '$name' already exists" -ForegroundColor Yellow
         return
-
     }
 
-    Try{
-        
+    Try {        
         New-ADOrganizationalUnit -Name $name -Path "DC=Globex,DC=com" -ProtectedFromAccidentalDeletion $false 
         Write-Host "Organizational Unit '$name' was successfully created." -ForegroundColor Green
-    
-    } Catch {
-    
-        Write-Host ("Failed to Create") -ForegroundColor Red 
-    
     }
-
+    Catch {
+        Write-Host "Failed to Create" -ForegroundColor Red 
+    }
 }
