e78f1c51cfb5b8cd2420871e20f84064fd472e1a
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2026-07-15 17:10:24 -0500

    Add files via upload

---
+++ Users_OU_Structure.ps1
@@ -0,0 +1,51 @@
+﻿if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Please run this script as Administrator."
+    return 
+}
+else {
+    Write-Host "Running as Administrator."
+}
+
+$ouPath = "DC=Globex,DC=com"
+$usersOU = @("Executives", "Finance", "Advisors", "HR", "IT", "Sales")
+
+
+function AD-Module-Installed{
+
+    if(-not(Get-Module -name ActiveDirectory )){
+        try {
+            Import-Module -Name ActiveDirectory
+            Write-Host "ActiveDirectory has been installed..."
+        }
+        catch {
+            Write-Host "Failed to Create..." -ForegroundColor Red
+        }
+    } 
+}
+
+function Create-OUs {
+
+
+    forEach($i in $usersOU) {
+        
+        
+        if (Get-ADOrganizationalUnit -Filter "$i -eq '$i'" -SearchBase $OUPath) {    
+            Write-Host "Organizational Unit '$i' already exists" -ForegroundColor Yellow
+            return
+        }
+    
+        Try {        
+            New-ADOrganizationalUnit -Name $i -Path $ouPath -ProtectedFromAccidentalDeletion $false 
+            Write-Host "Organizational Unit '$i' was successfully created." -ForegroundColor Green
+        } 
+        
+        Catch {
+            Write-Host "Failed to Create: $i" -ForegroundColor Red 
+        }   
+    
+    }
+
+}
+
+AD-Module-Installed
+Create-OUs
